e1a5cb1ff89987f553c9c1a4d3514b968bc6d47d
777uliahshafar <777uliahshafar@gmail.com>
2023-12-28 04:33:10 +0800

    tjknvimr

--- after/ftplugin/rnoweb.lua
+++ after/ftplugin/rnoweb.lua
@@ -18,7 +18,5 @@ map("n", "<localleader>rv", ':call RAction("viewobj")<CR>', opts)
 
 map("n", "<localleader>lp", "<Plug>RSyncFor")
 
-map("t", "jk", [[<C-\><C-n>]], opts) --Use jk for enter normal mode from terminal mode
-
 map("n", "<localleader>ln", "<Plug>RNextRChunk")
 map("n", "<localleader>lN", "<Plug>RPreviousRChunk")
