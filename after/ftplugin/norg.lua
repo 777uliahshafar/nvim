b3ed8640e8725c8d08fa84fc1e6c6a65a79fb50a
777uliahshafar <777uliahshafar@gmail.com>
2024-01-10 18:16:57 +0800

    norgcmp

--- after/ftplugin/norg.lua
+++ after/ftplugin/norg.lua
@@ -1,10 +1,40 @@
 vim.opt_local.linebreak = true
 -- vim.opt_local.conceallevel = 2
 
+local kind_icons = {
+  Text = "",
+  Snippet = "",
+  File = "",
+  Interface = "",
+  Unit = "",
+  Struct = "",
+  Event = "",
+  Value = "",
+  Keyword = "",
+  Constant = "",
+  Operator = "",
+}
+
 require("cmp").setup.buffer {
+
+  formatting = {
+    format = function(entry, vim_item)
+      vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
+      vim_item.menu = ({
+        -- nvim_lsp = "[LSP]",
+        luasnip = "[Snippet]",
+        path = "[Path]",
+        omni = (vim.inspect(vim_item.menu):gsub('%"', "")),
+        dictionary = "[Dict]",
+      })[entry.source.name]
+      return vim_item
+    end,
+  },
   sources = {
-    { name = "luasnip" },
+    -- { name = "nvim_lsp" },
+    { name = "luasnip", keyword_length = 2 },
     { name = "path" },
-    { name = "neorg" },
+    { name = "omni" },
+    { name = "dictionary", keyword_length = 2 }, -- spelllang must be active
   },
 }
