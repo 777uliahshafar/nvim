e96ff1216e097cc8ac02b185e33692899ca415ef
777uliahshafar <777uliahshafar@gmail.com>
2024-03-02 18:49:26 +0800

    rmd

--- lua/core/plugin_config/r.lua
+++
@@ -1,8 +0,0 @@
-vim.g.R_auto_start = 2
-vim.g.R_routnotab = 1
--- vim.g.R_external_term = 1
-
-vim.g.R_assign = 0 -- disable convertion from "_" into "<-"
-vim.g.R_args = { "--no-save", "--quiet" } -- args when starting R
-
-vim.g.R_df_viewer = "View(%s)" -- View a data.frame or matrix
---
+++ lua/core/plugin_config/rmd.lua
@@ -0,0 +1,17 @@
+local g = vim.g
+
+-- Nvim-R
+
+g.R_auto_start = 2
+g.R_routnotab = 1
+-- vim.g.R_external_term = 1
+g.R_assign = 0 -- disable convertion from "_" into "<-"
+g.R_args = { "--no-save", "--quiet" } -- args when starting R
+g.R_df_viewer = "View(%s)" -- View a data.frame or matrix
+
+-- vim-markdown
+g.vim_markdown_frontmatter = 1
+g.tex_conceal = ""
+g.vim_markdown_math = 1
+g.vim_markdown_auto_insert_bullets = 0
+g.vim_markdown_new_list_item_indent = 0
