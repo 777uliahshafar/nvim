91a968d27e1a1e7fa8cf30716e1ef09a8e9f06fb
777uliahshafar <777uliahshafar@gmail.com>
2024-04-23 18:47:27 +0800

    configalpha

--- lua/core/plugin_config/alpha.lua
+++ lua/core/plugin_config/alpha.lua
@@ -20,13 +20,13 @@ startify.section.top_buttons.val = {
   startify.button("e", " > New File", "<cmd>ene<CR>"),
   startify.button("a", "AAS essay", "<cmd>e D:/essay/subfiles/aasessay.tex<CR>"),
   startify.button("b", "Karakter", "<cmd>e D:/karakter/karakter.tex<CR>"),
-  startify.button("c", "BPI-en", "<cmd>e D:/essay/subfiles/esai-en.tex<CR>"),
+  startify.button("c", "BPI-en", "<cmd>e D:/proposal/subfiles/prop-en.tex<CR>"),
 }
 
 startify.section.bottom_buttons.val = {
   startify.button("SPC pff", " > Find File", "<cmd>Telescope find_files<CR>"),
   startify.button("SPC pfw", " > Find Word", "<cmd>Telescope live_grep<CR>"),
-  startify.button("z", " > Find Word", "<cmd>e C:/Users/uliah/AppData/Local/nvim/lua/core/plugin_config/alpha.lua<CR>"),
+  startify.button("z", " > Find Config", "<cmd>e C:/Users/uliah/AppData/Local/nvim/init.lua<CR>"),
 }
 
 alpha.setup(startify.opts)
