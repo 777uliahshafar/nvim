601d46608d70546ec41a92caf3f7270f28c2e792
777uliahshafar <777uliahshafar@gmail.com>
2024-04-25 10:08:28 +0800

    citrusmodfi

--- lua/core/colors.lua
+++ lua/core/colors.lua
@@ -15,7 +15,8 @@ require("gruvbox").setup {
 require("citruszest").setup {
   style = {
     -- This will change Constant foreground color and make it bold.
-    Normal = { fg = "#F8F8F2" },
+    Normal = { fg = "#b1b4b9" },
+    CursorLine = { bg = "#121212" },
   },
 }
 
