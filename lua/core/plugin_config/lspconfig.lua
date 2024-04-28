50f545c154f12a4f9e233a8bb849936274c37e0c
777uliahshafar <777uliahshafar@gmail.com>
2024-04-28 11:23:33 +0800

    nophp

--- lua/core/plugin_config/lspconfig.lua
+++ lua/core/plugin_config/lspconfig.lua
@@ -3,7 +3,7 @@ require("mason").setup()
 
 -- mason-lspconfig --
 require("mason-lspconfig").setup {
-  ensure_installed = { "lua_ls", "intelephense", "jsonls", "html", "cssls", "tsserver", "emmet_ls" },
+  ensure_installed = { "lua_ls", "jsonls", "html", "cssls", "tsserver", "emmet_ls" },
   automatic_installation = true,
 }
 
@@ -52,71 +52,6 @@ lspconfig.lua_ls.setup {
   },
 }
 
-lspconfig.intelephense.setup {
-  on_attach = on_attach,
-  capabilities = capabilities,
-  settings = {
-    intelephense = {
-      stubs = {
-        "bcmath",
-        "bz2",
-        "Core",
-        "curl",
-        "date",
-        "dom",
-        "fileinfo",
-        "filter",
-        "gd",
-        "gettext",
-        "hash",
-        "iconv",
-        "imap",
-        "intl",
-        "json",
-        "libxml",
-        "mbstring",
-        "mcrypt",
-        "mysql",
-        "mysqli",
-        "password",
-        "pcntl",
-        "pcre",
-        "PDO",
-        "pdo_mysql",
-        "Phar",
-        "readline",
-        "regex",
-        "session",
-        "SimpleXML",
-        "sockets",
-        "sodium",
-        "standard",
-        "superglobals",
-        "tokenizer",
-        "xml",
-        "xdebug",
-        "xmlreader",
-        "xmlwriter",
-        "yaml",
-        "zip",
-        "zlib",
-        "wordpress-stubs",
-        "woocommerce-stubs",
-        "acf-pro-stubs",
-        "wordpress-globals",
-        "wp-cli-stubs",
-        "genesis-stubs",
-        "polylang-stubs",
-      },
-      environment = {
-        includePaths = { "/home/mte90/.composer/vendor/php-stubs/", "/home/mte90/.composer/vendor/wpsyntex/" },
-      },
-      files = {
-        maxSize = 5000000,
-      },
-    },
-  },
-}
 lspconfig.jsonls.setup {
   on_attach = on_attach,
   capabilities = capabilities,
