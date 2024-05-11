4dcbb01bde319564b335011556e50343b9007fd3
777uliahshafar <777uliahshafar@gmail.com>
2024-05-11 11:51:02 +0800

    tranlationesc

--- lua/core/plugin_config/trans.lua
+++ lua/core/plugin_config/trans.lua
@@ -1,3 +1,15 @@
 require("pantran").setup {
   default_engine = "google",
+  controls = {
+    mappings = {
+      edit = {
+        n = {
+          -- Use this table to add additional mappings for the normal mode in
+          -- the translation window. Either strings or function references are
+          -- supported.
+          ["<Esc>"] = false,
+        },
+      },
+    },
+  },
 }
