ecac5960d3e58cb61b48931da0e845b89a1360e6
777uliahshafar <777uliahshafar@gmail.com>
2025-03-22 07:49:01 +0800

    harpoonsave

--- lua/core/plugin_config/harpoon.lua
+++ lua/core/plugin_config/harpoon.lua
@@ -1,7 +1,7 @@
 require("harpoon").setup {
   global_settings = {
     -- sets the marks upon calling `toggle` on the ui, instead of require `:w`.
-    save_on_toggle = true,
+    save_on_toggle = false,
 
     -- saves the harpoon file upon every change. disabling is unrecommended.
     save_on_change = true,
