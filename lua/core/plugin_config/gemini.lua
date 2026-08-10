e2d231a832e3b072e5228e35c4e55a4b3eb914d3
777uliahshafar <777uliahshafar@gmail.com>
2026-08-10 11:25:32 +0800

    geminicomple

--- lua/core/plugin_config/gemini.lua
+++ lua/core/plugin_config/gemini.lua
@@ -10,6 +10,41 @@ require("gemini").setup {
   },
   completion = {
     enabled = false,
+    blacklist_filetypes = { "help", "qf", "json", "yaml", "toml", "xml" },
+    blacklist_filenames = { ".env" },
+    completion_delay = 800,
+    insert_result_key = "<S-Tab>",
+    move_cursor_end = true,
+    can_complete = function()
+      return vim.fn.pumvisible() ~= 1
+    end,
+    get_system_text = function()
+      return "Bertindak sebagai penulis peneliti yang mempublikasikan artikel berakreditaasi nasional yang melengkapi secara otomatis kalimat pengguna."
+        .. "\n* Tugasmu memberikan saran kalimat pada posisi kursor ditandai dengan <cursor></cursor>."
+        .. "\n* Responmu tidak perlu menyediakan penjelasan."
+    end,
+    get_prompt = function(bufnr, pos)
+      local filetype = vim.api.nvim_get_option_value("filetype", { buf = bufnr })
+      local prompt = "Dibawah ini adalah konten dari  %s berkas `%s`:\n"
+        .. "```%s\n%s\n```\n\n"
+        .. "Sarankan kalimat yang paling mungkin pada  <cursor></cursor>.\n"
+      -- .. "Bungkus responmu pada ``` ```\n"
+      -- .. "eg.\n```\n```\n\n"
+      local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
+      local line = pos[1]
+      local col = pos[2]
+      local target_line = lines[line]
+      if target_line then
+        lines[line] = target_line:sub(1, col) .. "<cursor></cursor>" .. target_line:sub(col + 1)
+      else
+        return nil
+      end
+      local code = vim.fn.join(lines, "\n")
+      local abs_path = vim.api.nvim_buf_get_name(bufnr)
+      local filename = vim.fn.fnamemodify(abs_path, ":.")
+      prompt = string.format(prompt, filetype, filename, filetype, code)
+      return prompt
+    end,
   },
   instruction = {
     enabled = true,
