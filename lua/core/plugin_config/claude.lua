d59d4b6a4a925e7655fc9e9d9a922e42ff629344
777uliahshafar <777uliahshafar@gmail.com>
2026-09-15 14:03:40 +0800

    escsekali:

--- lua/core/plugin_config/claude.lua
+++ lua/core/plugin_config/claude.lua
@@ -15,6 +15,9 @@ require("claudecode").setup {
         -- Option 2: Use a custom local statusline string if desired
         statusline = "%{b:snacks_terminal.id}: %{b:term_title}",
       },
+      keys = {
+        term_normal = { "<esc>", "<C-\\><C-n>", mode = "t", desc = "Ke Normal Mode" },
+      }, -- tekan sekali untuk pindah mode, rentan masalah dengan cli lain pada terminal
     },
   },
 }
